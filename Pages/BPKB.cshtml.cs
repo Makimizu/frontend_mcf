5b6ecb8870205c06fab4d2e1c63e00d8ffb6404f
heruprasetya <heruprasetyaaa@gmail.com>
2022-10-17 17:53:14 +0700

    Add project files.

---
+++ Pages/BPKB.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace frontend_mcf.Pages
+{
+    public class BPKBModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
